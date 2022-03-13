b841fc053d0790ddb49045105dae9251f48991fc
albadauto <joseadauto923@gmail.com>
2022-03-13 18:03:17 -0300

    CRUD CORRECTION

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -14,12 +14,7 @@ const Form = () => {
         api.get('/').then((res) => setSearch(res.data));
     }, [])
 
-    const onchange = (ev) => {
-        const { name, value } = ev.target
-
-        setNome({ ...nome, [name]: value })
-
-    }
+    
 
     const onsubmit = async (ev) => {
         try{
@@ -39,23 +34,22 @@ const Form = () => {
         })
     }
 
-
     return ( 
         <>
         <form onSubmit={onsubmit} method="post">
             Nome: <br /> 
-            <input type="text" name="nome"  className='form-control' onChange={onchange}/><br />
+            <input type="text" name="nome" value={nome.nome} className='form-control' onChange={(e) => setNome({...nome, nome: e.target.value})}/><br />
             Sobrenome: <br />
-            <input type="text" name="sobrenome" id="" className='form-control' onChange={onchange}/><br />
+            <input type="text" name="sobrenome" value={nome.sobrenome} className='form-control' onChange={(e) => setNome({...nome, sobrenome: e.target.value})}/><br />
             cpf: <br />
-            <input type="text" name="cpf" id="" className='form-control' onChange={onchange}/><br />
+            <input type="text" name="cpf" id="" value={nome.cpf} className='form-control' onChange={(e) => setNome({...nome, cpf: e.target.value})}/><br />
             <button className='btn btn-success' type='submit'>Enviar</button>
         </form><br />
 
         {search.map((res, key) => {
             return(
                 <div className="teste">
-                    <label htmlFor="" key={Math.random()} className="alert alert-danger">{res.nome}</label>
+                    <label htmlFor="" key={Math.random()} className="alert alert-danger">{res.nome} {res.sobrenome}</label>
                     *<button value={res._id} className="btn btn-danger" onClick={deleteById}>Deletar</button>
                 </div>
             )
